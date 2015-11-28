ed06d27e5f54a15e1d4f73e3be0ec2762dc64061
fengzhongnanzi <rp599207211@gmail.com>
2015-11-28 21:12:07 +0800

    Initial commit

---
+++ mybatis_generator/src/Main.java
@@ -0,0 +1,34 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015-11-02.
+ */
+public class Main {
+    public void generator() throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+//        指定逆向工程配置文件
+        File configFile = new File("G:\\IDEA\\workspace\\mybatis_generator\\generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+    public static void main(String[] args){
+        try {
+            Main generatorSqlmap=new Main();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
